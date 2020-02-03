945fa7318ab7c4b444dffd5a2bbdcea45ef3aa0d
Facundo Petre <fpetre@paypal.com>
2020-02-03 12:00:11 -0300

    refactor Animated destructuring

--- components/useSwipe.ts
+++ components/useSwipe.ts
@@ -1,24 +1,33 @@
-import { useState } from "react";
-import { Dimensions, StyleSheet } from "react-native";
-import Animated from "react-native-reanimated";
-import { State } from "react-native-gesture-handler";
+import { useState } from 'react'
+import { Dimensions, StyleSheet } from 'react-native'
+import Animated from 'react-native-reanimated'
+import { State } from 'react-native-gesture-handler'
+const {
+  cond,
+  spring,
+  set,
+  clockRunning,
+  startClock,
+  Value,
+  stopClock,
+  eq,
+  Clock,
+  and,
+  lessThan,
+  greaterThan,
+  neq,
+  call,
+  multiply,
+  add,
+} = Animated
 
 function runSpring(clock, value, dest) {
-  const {
-    cond,
-    spring,
-    set,
-    clockRunning,
-    startClock,
-    Value,
-    stopClock
-  } = Animated;
   const state = {
     finished: new Value(0),
     velocity: new Value(0),
     position: new Value(0),
-    time: new Value(0)
-  };
+    time: new Value(0),
+  }
 
   const config = {
     damping: 20,
@@ -27,8 +36,8 @@ function runSpring(clock, value, dest) {
     overshootClamping: false,
     restSpeedThreshold: 1,
     restDisplacementThreshold: 0.5,
-    toValue: new Value(0)
-  };
+    toValue: new Value(0),
+  }
 
   return [
     cond(clockRunning(clock), 0, [
@@ -36,62 +45,47 @@ function runSpring(clock, value, dest) {
       set(state.velocity, 0),
       set(state.position, value),
       set(config.toValue, dest),
-      startClock(clock)
+      startClock(clock),
     ]),
     spring(clock, state, config),
     cond(state.finished, stopClock(clock)),
-    state.position
-  ];
+    state.position,
+  ]
 }
 
 const useSwipe = callback => {
-  const { width, height } = Dimensions.get("window");
-  const [translationX] = useState(new Animated.Value(0));
-  const [translationY] = useState(new Animated.Value(0));
-  const [velocityX] = useState(new Animated.Value(0));
-  const [gestureState] = useState(new Animated.Value(State.UNDETERMINED));
-  const [offsetX] = useState(new Animated.Value(0));
-  const [offsetY] = useState(new Animated.Value(0));
-  const {
-    cond,
-    eq,
-    set,
-    Clock,
-    and,
-    lessThan,
-    greaterThan,
-    clockRunning,
-    neq,
-    call,
-    multiply,
-    add,
-    stopClock
-  } = Animated;
-  const clockX = new Clock();
-  const clockY = new Clock();
+  const { width, height } = Dimensions.get('window')
+  const [translationX] = useState(new Value(0))
+  const [translationY] = useState(new Value(0))
+  const [velocityX] = useState(new Value(0))
+  const [gestureState] = useState(new Value(State.UNDETERMINED))
+  const [offsetX] = useState(new Value(0))
+  const [offsetY] = useState(new Value(0))
+  const clockX = new Clock()
+  const clockY = new Clock()
   const restoreDefault = () => {
-    offsetY.setValue(0);
-    offsetX.setValue(0);
-    velocityX.setValue(0);
-    translationX.setValue(0);
-    translationY.setValue(0);
+    offsetY.setValue(0)
+    offsetX.setValue(0)
+    velocityX.setValue(0)
+    translationX.setValue(0)
+    translationY.setValue(0)
     // TODO: Check this type issue
-    gestureState.setValue(Animated.UNDETERMINED);
-  };
-  restoreDefault();
+    gestureState.setValue(Animated.UNDETERMINED)
+  }
+  restoreDefault()
 
-  const translationThreshold = width / 4;
-  const finalTranslateX = add(translationX, multiply(0.5, velocityX));
+  const translationThreshold = width / 4
+  const finalTranslateX = add(translationX, multiply(0.5, velocityX))
   const onSwiped = ([translateX]) => {
-    const isRight = translateX > 0;
-    callback && callback(isRight);
-    restoreDefault();
-  };
+    const isRight = translateX > 0
+    callback && callback(isRight)
+    restoreDefault()
+  }
   const snapPoint = cond(
     lessThan(finalTranslateX, -translationThreshold),
     -(width + 100),
     cond(greaterThan(finalTranslateX, translationThreshold), width + 100, 0)
-  );
+  )
 
   const tempTranslationX = cond(
     eq(gestureState, State.END),
@@ -99,54 +93,54 @@ const useSwipe = callback => {
       set(translationX, runSpring(clockX, translationX, snapPoint)),
       set(offsetX, translationX),
       cond(and(eq(clockRunning(clockX), 0), neq(translationX, 0)), [
-        call([translationX], onSwiped)
+        call([translationX], onSwiped),
       ]),
-      translationX
+      translationX,
     ],
     cond(
       eq(gestureState, State.BEGAN),
       [stopClock(clockX), translationX],
       translationX
     )
-  );
+  )
 
   const tempTranslationY = cond(
     eq(gestureState, State.END),
     [
       set(translationY, runSpring(clockY, translationY, 0)),
       set(offsetY, translationY),
-      translationY
+      translationY,
     ],
     cond(
       eq(gestureState, State.BEGAN),
       [stopClock(clockY), translationY],
       translationY
     )
-  );
+  )
 
   const likeOpacity = Animated.interpolate(translationX, {
     inputRange: [0, width / 4],
     outputRange: [0, 1],
-    extrapolate: Animated.Extrapolate.CLAMP
-  });
+    extrapolate: Animated.Extrapolate.CLAMP,
+  })
   const nopeOpacity = Animated.interpolate(translationX, {
     inputRange: [-width / 4, 0],
     outputRange: [1, 0],
-    extrapolate: Animated.Extrapolate.CLAMP
-  });
+    extrapolate: Animated.Extrapolate.CLAMP,
+  })
   const superLikeOpacity = Animated.interpolate(translationY, {
     inputRange: [-height / 4, 0],
     outputRange: [1, 0],
-    extrapolate: Animated.Extrapolate.CLAMP
-  });
+    extrapolate: Animated.Extrapolate.CLAMP,
+  })
   const rotateZ = Animated.concat(
     Animated.interpolate(translationX, {
       inputRange: [-width / 2, width / 2],
       outputRange: [15, -15],
-      extrapolate: Animated.Extrapolate.CLAMP
+      extrapolate: Animated.Extrapolate.CLAMP,
     }),
-    "deg"
-  );
+    'deg'
+  )
   const onGestureEvent = Animated.event(
     [
       {
@@ -154,31 +148,31 @@ const useSwipe = callback => {
           translationX,
           translationY,
           velocityX,
-          state: gestureState
-        }
-      }
+          state: gestureState,
+        },
+      },
     ],
     {
-      useNativeDriver: true
+      useNativeDriver: true,
     }
-  );
+  )
   const style = {
     ...StyleSheet.absoluteFillObject,
     zIndex: 900,
     transform: [
       { translateX: tempTranslationX },
       { translateY: tempTranslationY },
-      { rotateZ }
-    ]
-  };
+      { rotateZ },
+    ],
+  }
 
   return {
     style,
     onGestureEvent,
     likeOpacity,
     nopeOpacity,
-    superLikeOpacity
-  };
-};
+    superLikeOpacity,
+  }
+}
 
-export default useSwipe;
+export default useSwipe
