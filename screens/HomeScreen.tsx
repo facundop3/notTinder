3f350322f2e9cddaa53c45cbdf47087674aa43dd
Facundo Petre <facu1p3@gmail.com>
2020-04-20 08:48:19 -0300

    Wip improve ui stacking swipeable media cards

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -12,19 +12,22 @@ import {
 // import Radar from "../components/Radar";
 import TopNavigation from "../navigation/TopNavigator";
 import ActionButtons from "../components/ActionButtons";
-import { candidatesList as candidates } from "../sampleData";
+import { candidatesList } from "../sampleData";
 import { getNearbyUsers } from "../utils";
 import { SafeAreaModal, MediaCard, SwipeableWrapper } from 'nottinderuikit'
 import CandidateProfile from '../components/CandidateProfile'
 import DataPreview from '../components/DataPreview'
 function HomeScreen(props) {
   const { width, height } = Dimensions.get("window");
-  const candidateCardPosition = new Animated.ValueXY();
-  const [currentCandidatendex, setCurrentCardIndex] = useState(0);
+  const [candidatesAndPositions, setCandidatesAndPositions] = useState(candidatesList.map(candidate => ({ candidate, position: new Animated.ValueXY() })));
+  const [currentCandidateIndex, setCurrentCandidateIndex] = useState(candidatesAndPositions.length - 1)
   const [isGlodPage, setIsGoldPage] = useState<boolean>(false);
   const [showCandidateModal, setShowCandidateModal] = useState(false);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const nextCandidate = () => setCurrentCardIndex((currentCandidatendex + 1) % candidates.length)
+  const nextCandidate = () => {
+    candidatesAndPositions.pop()
+    setCandidatesAndPositions([...candidatesAndPositions])
+  }
   const toggleCandidateModal = () => setShowCandidateModal(!showCandidateModal);
   const nextCurrentImageIndex = (n, length) => {
     const nextIndex = n + currentImageIndex;
@@ -47,7 +50,7 @@ function HomeScreen(props) {
   }
 
   const verticalSwipe = () => {
-    Animated.timing(candidateCardPosition, {
+    Animated.timing(candidatesAndPositions[0].position, {
       toValue: { x: 0, y: -height },
       duration: 500,
     }).start(() => {
@@ -58,7 +61,7 @@ function HomeScreen(props) {
 
   const horizontalSwipe = (isLeftToRight = false) => {
     const translateX = (isLeftToRight ? 1 : -1) * width * 1.5;
-    Animated.timing(candidateCardPosition, {
+    Animated.timing(candidatesAndPositions[0].position, {
       toValue: { x: translateX, y: 0 },
       duration: 400,
     }).start(() => {
@@ -68,15 +71,19 @@ function HomeScreen(props) {
   };
 
   const resetPosition = (delay: number = 0) => {
-    Animated.timing(candidateCardPosition, {
-      toValue: { x: 0, y: 0 },
-      duration: 250,
-      delay
-    }).start();
+    // Animated.timing(candidateCardPosition, {
+    //   toValue: { x: 0, y: 0 },
+    //   duration: 250,
+    //   delay
+    // }).start();
   };
   const navTo = (page: string, params: object = {}) => {
     props.navigation.navigate(page, params);
   };
+  useEffect(() => {
+    console.log('rendered')
+    console.log(candidatesAndPositions.map(({ candidate: { name } }) => name))
+  })
 
   useEffect(() => {
     getNearbyUsers()
@@ -85,14 +92,13 @@ function HomeScreen(props) {
   }, []);
 
   useEffect(() => {
-    // Restores initial candidateCardPosition when currentCardIndex changes
-    // So the next candidate shows in the initial position :)
-    resetPosition(200)
-  }, [currentCandidatendex]);
+    setCurrentImageIndex(0)
+  }, [candidatesAndPositions])
+
   return (
     <>
       <SafeAreaView style={styles.safeView}>
-        <View style={styles.contentContainer}>
+        <View>
           <TopNavigation
             navTo={navTo}
             switchValue={isGlodPage}
@@ -101,22 +107,32 @@ function HomeScreen(props) {
           {!isGlodPage && (
             <>
               <View style={styles.cardContainer}>
-                <SwipeableWrapper
-                  verticalCallback={verticalCallback}
-                  horizontalCallback={horizontalCallback}
-                  animatedDefaultPosition={candidateCardPosition}
-                >
-                  <MediaCard
-                    animatedCardPosition={candidateCardPosition}
-                    leftLabel="Like"
-                    rightLabel="Nope"
-                    downLabel="Super Like"
-                    onBottomPress={toggleCandidateModal}
-                    images={candidates[currentCandidatendex].pictures}
-                    currentImageIndex={currentImageIndex}
-                    handleCurrentImageChange={nextCurrentImageIndex}
-                    bottomData={<DataPreview data={candidates[currentCandidatendex]} />} />
-                </SwipeableWrapper>
+                {
+                  candidatesAndPositions.map(({ candidate, position }) => (
+                    <View key={candidate.id} style={{
+                      position: "absolute",
+                      width: "100%",
+                      height: '100%'
+                    }}>
+                      <SwipeableWrapper
+                        verticalCallback={verticalCallback}
+                        horizontalCallback={horizontalCallback}
+                        animatedDefaultPosition={position}
+                      >
+                        <MediaCard
+                          animatedCardPosition={position}
+                          leftLabel="Like"
+                          rightLabel="Nope"
+                          downLabel="Super Like"
+                          onBottomPress={toggleCandidateModal}
+                          images={candidate.pictures}
+                          currentImageIndex={currentImageIndex}
+                          handleCurrentImageChange={nextCurrentImageIndex}
+                          bottomData={<DataPreview data={candidate} />} />
+                      </SwipeableWrapper>
+                    </View>
+                  ))
+                }
                 {/* <Radar /> */}
               </View>
               <ActionButtons
@@ -130,7 +146,7 @@ function HomeScreen(props) {
       </SafeAreaView>
       <SafeAreaModal visible={showCandidateModal}>
         <CandidateProfile
-          data={candidates[currentCandidatendex]}
+          data={candidatesAndPositions[candidatesAndPositions.length - 1].candidate}
           isModal
           toggleCandidateModal={toggleCandidateModal}
           nextCurrentImageIndex={nextCurrentImageIndex}
@@ -146,21 +162,13 @@ HomeScreen.navigationOptions = {
 };
 
 const styles = StyleSheet.create<any>({
-  contentContainer: {
-    height: "100%"
-  },
   safeView: {
     paddingTop: Platform.OS === "android" ? 25 : 0,
   },
   cardContainer: {
     height: "80%",
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    lineHeight: 24,
-    textAlign: "center",
-  },
+    position: "relative"
+  }
 });
 
 export default HomeScreen;
